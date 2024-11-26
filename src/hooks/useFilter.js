587862e030994dedfc716989b421e71ca94f9bd2
AyanCse39 <ayan48cse@gmail.com>
2024-11-26 16:46:56 +0530

    After filter

--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -2,7 +2,8 @@ import { useState } from "react"
 
 export function useFilter(dataList,callbcak) {
     const [query,setQuery]= useState('')
-  const  filteredData= dataList.filter((data)=>  callbcak(data).toLowerCase().includes(query))
+  const  filteredData= dataList.filter((data)=>  
+    callbcak(data).toLowerCase().includes(query))
     
     return [filteredData, setQuery]
 
