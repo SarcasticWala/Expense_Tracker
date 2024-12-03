c74195c907dbcabb82f6f3a18115964e3ebbc223
AyanCse39 <ayan48cse@gmail.com>
2024-12-03 18:24:23 +0530

    Finnal Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import './App.css'
 import ExpenseForm from './components/ExpenseForm'
 import ExpenseTable from './components/ExpenseTable'
 import expenseData from './expenseData'
+import { useLocalStorage } from './hooks/useLocalStorage'
 
 function App() {
-  const [expense, setExpense] = useState({
+  const [expense, setExpense] = useLocalStorage('expense', {
     title: '',
     category: '',
     amount: '',
   })
-  const [expenses, setExpenses] = useState(expenseData)
-  const [editingRowId, setEditingRowId] = useState('')
+  const [expenses, setExpenses] = useLocalStorage('expenses', expenseData)
+  const [editingRowId, setEditingRowId] = useLocalStorage('editingRowId', '')
 
   return (
     <main>
--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,10 +1,12 @@
-import { useState } from "react"
+import { useState } from 'react'
+import { useLocalStorage } from './useLocalStorage'
 
-export function useFilter(dataList,callbcak) {
-    const [query,setQuery]= useState('')
-  const  filteredData= dataList.filter((data)=>  
-    callbcak(data).toLowerCase().includes(query))
-    
-    return [filteredData, setQuery]
+export function useFilter(dataList, callback) {
+  const [query, setQuery] = useLocalStorage('query', '')
 
+  const filteredData = dataList.filter((data) =>
+    callback(data).toLowerCase().includes(query)
+  )
+
+  return [filteredData, setQuery]
 }
